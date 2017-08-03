28662cba0e2d937ca3729d41ea8cb4d977f67628
Michael Kuenzli <michael.kuenzli@camptocamp.com>
2017-08-03 16:20:56 +0200

    Move routing options to single object: gmfRoutingOptions

--- contribs/gmf/options/gmfx.js
+++ contribs/gmf/options/gmfx.js
@@ -725,3 +725,12 @@ gmfx.RoutingVia;
  * }}
  */
 gmfx.RoutingProfile;
+
+/**
+ * @typedef {{
+ *     backendUrl: (string|undefined),
+ *     profiles: (Array.<gmfx.RoutingProfile>|undefined),
+ *     searchDefaultParams: (Object.<string, string>|undefined)
+ * }}
+ */
+gmfx.RoutingOptions;
